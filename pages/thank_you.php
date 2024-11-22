6ea344fced59d190005843cae1108d90d03db534
lucasgiorgi95 <lucas1995giorgi@gmail.com>
2024-11-22 10:49:50 -0300

    fix thank page

--- pages/thank_you.php
+++ pages/thank_you.php
@@ -25,45 +25,60 @@ Template Name: Thank You
             height: 100%;
             width: 100%;
         }
+        .content-wrapper {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            height: 100vh;
+            text-align: center;
+        }
+        .logo-container {
+            position: absolute;
+            top: 12px;
+            left: 12px;
+        }
     </style>
 </head>
 
 <body class="m-0 p-0">
     <!-- Fondo con imagen centrada y cubriendo todo el espacio -->
     <div class="min-h-screen bg-center bg-no-repeat bg-origin-content" style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/Assetss/ThankYou/TYP_IMG.webp');">
-        <!-- Ajuste del logo en la esquina superior izquierda -->
-        <div class="absolute top-12 left-12">
+        
+        <!-- Contenedor para el logo en la esquina superior izquierda -->
+        <div class="logo-container">
             <img src="<?php echo get_stylesheet_directory_uri(); ?>/Assetss/Landing/Header_Logo.webp" alt="Logo de la empresa" class="w-52">
         </div>
 
-        <!-- Texto principal en el centro -->
-        <div class="flex flex-col justify-center items-center h-full"> <!-- Contenedor flex -->
-            <h1 class="text-4xl md:text-5xl text-white font-Montse font-bold pb-20 md:mt-[12rem]"> <!-- Ajuste de margen -->
+        <!-- Contenedor principal centrado -->
+        <div class="content-wrapper">
+            <!-- Texto principal en el centro -->
+            <h1 class="text-4xl md:text-5xl text-white font-Montse font-bold pb-20 md:mt-[12rem]">
                 <span class="text-red-500">¡GRACIAS</span> POR QUERER
                 <span class="text-red-500"> BAILAR</span><br>
                 <span class="text-white">CON NOSOTROS!</span>
             </h1>
-        </div>
 
-        <!-- Caja negra transparente con texto -->
-        <div class="absolute bottom-6 left-1/2 text-center transform -translate-x-1/2 bg-black bg-opacity-60 text-white p-10 rounded-lg max-w-4xl w-full md:w-3/4">
-            <p class="text-base md:text-lg font-Montse">
-                Pronto estaremos sacando los mejores pasos en la pista de baile junto con las mejores estrategias para que tu marca genere emoción.
-            </p>
-            <span class="font-bold text-white"> Estamos en contacto</span>
+            <!-- Caja negra transparente con texto -->
+            <div class="bg-black bg-opacity-60 text-white p-10 rounded-lg max-w-4xl w-full md:w-3/4">
+                <p class="text-base md:text-lg font-Montse">
+                    Pronto estaremos sacando los mejores pasos en la pista de baile junto con las mejores estrategias para que tu marca genere emoción.
+                </p>
+                <span class="font-bold text-white"> Estamos en contacto</span>
 
-            <!-- Iconos de redes sociales -->
-            <h3 class="font-bold text-lg md:text-xl text-red-500 mt-[2rem] font-Montse">Sigue nuestro baile:</h3>
-            <div class="flex justify-center space-x-4 mt-4">
-                <a href="https://www.instagram.com/pachucolens/" target="_blank">
-                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/Assetss/ThankYou/TYP_Icon_Instagram.webp" alt="Instagram" class="w-8 h-8">
-                </a>
-                <a href="https://www.tiktok.com/@pachuco_digital" target="_blank">
-                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/Assetss/ThankYou/TYP_Icon_Tk.webp" alt="TikTok" class="w-8 h-8">
-                </a>
-                <a href="https://www.linkedin.com/company/pachuco-digital/mycompany/" target="_blank">
-                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/Assetss/ThankYou/TYP_Icon_Linedin.webp" alt="LinkedIn" class="w-8 h-8">
-                </a>
+                <!-- Iconos de redes sociales -->
+                <h3 class="font-bold text-lg md:text-xl text-red-500 mt-[2rem] font-Montse">Sigue nuestro baile:</h3>
+                <div class="flex justify-center space-x-4 mt-4">
+                    <a href="https://www.instagram.com/pachucolens/" target="_blank">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/Assetss/ThankYou/TYP_Icon_Instagram.webp" alt="Instagram" class="w-8 h-8">
+                    </a>
+                    <a href="https://www.tiktok.com/@pachuco_digital" target="_blank">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/Assetss/ThankYou/TYP_Icon_Tk.webp" alt="TikTok" class="w-8 h-8">
+                    </a>
+                    <a href="https://www.linkedin.com/company/pachuco-digital/mycompany/" target="_blank">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/Assetss/ThankYou/TYP_Icon_Linedin.webp" alt="LinkedIn" class="w-8 h-8">
+                    </a>
+                </div>
             </div>
         </div>
     </div>
